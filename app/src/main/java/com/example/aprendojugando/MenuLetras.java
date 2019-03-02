41bbb637d028d4c521975d49c25b1e827adab9f4
manicalavera <46912347+manicalavera@users.noreply.github.com>
2019-03-02 17:21:47 -0300

    Se agrega el menú letras
    Todavía no pude ubicar bien los botones porque el sistema de distancias es confuso

--- app/src/main/java/com/example/aprendojugando/MenuLetras.java
+++ app/src/main/java/com/example/aprendojugando/MenuLetras.java
@@ -5,58 +5,67 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MenuLetras extends AppCompatActivity {
 
     SoundPool sp;
     int sonido_de_reproduccion;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_menu_letras);
 
         //se encarga de cargar el sonido del botón para después reproduciorlo
         sp= new SoundPool(1, AudioManager.STREAM_MUSIC, 1);
         sonido_de_reproduccion= sp.load(this, R.raw.selec, 1);
-
-        //Poner el ícono al lado de action bar
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
-
     }
 
-    public void Niveles (View view){
+    public void Home (View view){
 
         //Parte que le da sonido al botón
         sp.play (sonido_de_reproduccion, 1, 1, 1, 0, 0);
 
-        //linkea al menu niveles
+        //linkea al menu principal
 
-        Intent siguiente = new Intent(this, MenuNiveles.class);
+        Intent siguiente = new Intent(this, MainActivity.class);
         startActivity(siguiente);
 
     }
-    public void Letras (View view){
+
+
+    public void A   (View view){
 
         //Parte que le da sonido al botón
         sp.play (sonido_de_reproduccion, 1, 1, 1, 0, 0);
 
-        Intent siguiente = new Intent(this, MenuLetras.class);
-        startActivity(siguiente);
+    }
 
+    public void E (View view){
 
+        //Parte que le da sonido al botón
+        sp.play (sonido_de_reproduccion, 1, 1, 1, 0, 0);
 
     }
-    public void Acercade (View view){
+    public void I (View view){
+
+        //Parte que le da sonido al botón
+        sp.play (sonido_de_reproduccion, 1, 1, 1, 0, 0);
+
+    }
+    public void O (View view){
+
+        //Parte que le da sonido al botón
+        sp.play (sonido_de_reproduccion, 1, 1, 1, 0, 0);
+
+    }
+    public void U (View view){
 
         //Parte que le da sonido al botón
         sp.play (sonido_de_reproduccion, 1, 1, 1, 0, 0);
 
     }
 }
+
